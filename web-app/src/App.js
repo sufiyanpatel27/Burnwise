621a37e12d55d98349a671d0704b5641e057f21a
sufiyan <sp3596894@gmail.com>
2021-08-16 13:05:36 +0530

    regression model worked

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -21,6 +21,9 @@ function App() {
   const slope = (x1, y1, x2, y2) => {
     return (y2 - y1) / (x2 - x1);
   }
+  const distance = (x1, y1, x2, y2) => {
+    return Math.pow(Math.pow(y2 - y1, 2) + Math.pow(x2 - x1, 2), 0.5)
+  }
   const roundoff = (x1, y1, x2, y2) => {
     const s = slope(x1, y1, x2, y2);
     const d = 1
@@ -29,6 +32,13 @@ function App() {
     //return -round(x, 3), -round(y, 3)
     return [-x.toFixed(3), -y.toFixed(3)]
   }
+  const retrivew = (x1, y1, x2, y2, x3, y3, x4, y4) => {
+    const s = slope(x1, y1, x2, y2)
+    const d = distance(x3, y3, x4, y4)
+    const x = Math.pow(d * (1 / (1 + Math.pow(s, 2))), 0.5) + x3
+    const y = s * (x - x3) + y3
+    return [x, y]
+  }
 
 
   const runHandpose = async () => {
@@ -36,9 +46,11 @@ function App() {
     console.log('Handpose model loaded.');
     const model = await tf.loadLayersModel('https://raw.githubusercontent.com/sufiyanpatel27/exercise_tracker/models/d_pull_ups/classifier/model.json')
     console.log('classification model loaded')
+    const model2 = await tf.loadLayersModel('https://raw.githubusercontent.com/sufiyanpatel27/exercise_tracker/models/d_pull_ups/regressor/model.json')
+    console.log('regression model loaded')
     try {
       setInterval(() => {
-        detect(detector, model)
+        detect(detector, model, model2)
       }, 1)
     } catch (error) {
       console.log('nothing')
@@ -47,7 +59,7 @@ function App() {
 
   runHandpose();
 
-  const detect = async (m_model, c_model) => {
+  const detect = async (m_model, c_model, r_model) => {
     if (
       typeof webcamRef.current !== 'undefined' &&
       webcamRef.current !== null &&
@@ -73,7 +85,7 @@ function App() {
         //[hand[0].keypoints[6].x, hand[0].keypoints[6].y],
         [hand[0].keypoints[7].x, hand[0].keypoints[7].y],
         //[hand[0].keypoints[8].x, hand[0].keypoints[8].y],
-        //[hand[0].keypoints[9].x, hand[0].keypoints[9].y],
+        [hand[0].keypoints[9].x, hand[0].keypoints[9].y],
         //[hand[0].keypoints[10].x, hand[0].keypoints[10].y],
       ]
 
@@ -88,8 +100,24 @@ function App() {
       let input_xs = tf.tensor2d([[x[0], x[1]]]);
       let output = c_model.predict(input_xs);
       const outputData = output.dataSync();
-      console.log(outputData)
+      //console.log(outputData)
 
+
+      // wrist prediction
+      const ans = r_model.predict(input_xs)
+      const outputData_1 = ans.dataSync();
+      //console.log(outputData_1[0], outputData_1[1]);
+      const x_reg = retrivew(x[0], x[1], outputData_1[0], outputData_1[1], hand[0].keypoints[7].x, hand[0].keypoints[7].y, hand[0].keypoints[9].x, hand[0].keypoints[9].y)
+      //console.log(x_reg[0], x_reg[1])
+      if ((x_reg[0] - hand[0].keypoints[9].x).toFixed(2) > 0.1) {
+        console.log('right')
+      }
+      else if ((x_reg[0] - hand[0].keypoints[9].x).toFixed(2) < 0) {
+        console.log('left')
+      }
+      else {
+        console.log('good')
+      }
     }
   }
 
